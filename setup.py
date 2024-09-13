f11b657ce0e6145d030d436732f0e4a3cd629bdb
slactjohnson <tjohnson@slac.stanford.edu>
2024-09-13 10:32:53 -0700

    Commit pre-commit fixes

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import sys
 from os import path
 
-import versioneer
 from setuptools import find_packages, setup
+
+import versioneer
 
 min_version = (3, 9)
 
@@ -17,40 +18,45 @@ This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
 Upgrade pip like so:
 
 pip install --upgrade pip
-""".format(*sys.version_info[:2], *min_version)
+""".format(
+        *sys.version_info[:2], *min_version
+    )
     sys.exit(error)
 
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+with open(path.join(here, "README.rst"), encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open(path.join(here, 'requirements.txt')) as requirements_file:
+with open(path.join(here, "requirements.txt")) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
+    requirements = [
+        line
+        for line in requirements_file.read().splitlines()
+        if not line.startswith("#")
+    ]
 
 
-git_requirements = [r for r in requirements if r.startswith('git+')]
+git_requirements = [r for r in requirements if r.startswith("git+")]
 if git_requirements:
-    print('User must install the following packages manually:')
+    print("User must install the following packages manually:")
     print()
-    print("\n".join(f'* {r}' for r in git_requirements))
+    print("\n".join(f"* {r}" for r in git_requirements))
     print()
 
 
 setup(
-    name='las_dispersion_scan',
+    name="las_dispersion_scan",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    license='BSD',
-    author='SLAC National Accelerator Laboratory',
-    packages=find_packages(exclude=['docs', 'tests']),
-    description='Laser dispersion scan utilities for the LCLS',
+    license="BSD",
+    author="SLAC National Accelerator Laboratory",
+    packages=find_packages(exclude=["docs", "tests"]),
+    description="Laser dispersion scan utilities for the LCLS",
     long_description=readme,
     long_description_content_type="text/x-rst",
-    url='https://github.com/pcdshub/las-dispersion-scan',  # noqa
+    url="https://github.com/pcdshub/las-dispersion-scan",  # noqa
     entry_points={
         "console_scripts": [
             "las-dispersion-scan=las_dispersion_scan.bin.main:main",
@@ -58,7 +64,7 @@ setup(
     },
     include_package_data=True,
     package_data={
-        'las_dispersion_scan': [
+        "las_dispersion_scan": [
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
             # 'path/to/data_file',
@@ -66,8 +72,8 @@ setup(
     },
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
     ],
 )
