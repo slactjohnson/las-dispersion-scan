951265e276c22e616c1b079d2b3217e58525c82c
slactjohnson <tjohnson@slac.stanford.edu>
2025-04-24 12:55:34 -0700

    Remove old python dev path from run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ source /reg/g/pcds/engineering_tools/latest-released/scripts/pcds_conda ""
 cd "$SCRIPT_DIR" || exit 1
 
 export PYTHONPATH=/cds/group/pcds/pyps/apps/dev/pythonpath:$PYTHONPATH
-export PYTHONPATH=/cds/home/t/tjohnson/trunk/hutch-python/forks/pcdsdevices:$PYTHONPATH
+#export PYTHONPATH=/cds/home/t/tjohnson/trunk/hutch-python/forks/pcdsdevices:$PYTHONPATH
 
 # unset __GLX_VENDOR_LIBRARY_NAME
 python -m las_dispersion_scan "$@"
